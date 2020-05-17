dd58bbd423fbd249b9f14eb86bdf5e558d9bdd6b
sharkyak <a.kazakov@computer-ufa.ru>
2020-05-17 19:15:01 +0500

    client build

---
+++ client/build/precache-manifest.e8c64fe5d0ac5e4fdbc36583e36a5fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8e8fdcadc27ae700b2e7cff272fb5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31d7af12eb1959c19e9c",
+    "url": "/static/css/main.1cba1bc6.chunk.css"
+  },
+  {
+    "revision": "74f95002798abb653709",
+    "url": "/static/js/2.d22f6b91.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d22f6b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d7af12eb1959c19e9c",
+    "url": "/static/js/main.34dfcc5b.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
